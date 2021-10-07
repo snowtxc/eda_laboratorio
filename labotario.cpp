f15cff9fb3bf844b79a7a6520cea540404e3ae2a
Rodrigo Castro <snowitxc@gmail.com>
2021-10-06 21:51:46 -0300

    define structures

--- labotario.cpp
+++ labotario.cpp
@@ -2,9 +2,47 @@
 
 using namespace std;
 
-int main()
-{
-    cout<<"Hello World";
 
-    return 0;
+struct nodoPalabra{
+    string palabra;
+    nodoPalabra * sigPalabra;
+};
+
+
+struct nodoLinea{
+    nodoLinea * sig;
+    nodoPalabra * sigPalabra;
+};
+
+typedef nodoPalabra * ListaPalabra;
+typedef nodoLinea * ListaLinea;
+
+ListaLinea lista = NULL;
+
+
+
+string InsertarLineaEnPosicion(int posicionLinea){
+    return "NO IMPLEMENTADA";
+}
+
+
+
+
+int main()
+{ 
+   ListaLinea nodo1 = new nodoLinea;
+   nodo1->sigPalabra = NULL;
+
+   ListaPalabra palabra1 =  new nodoPalabra;
+   palabra1->palabra = "Gato";
+
+   nodo1->sigPalabra = palabra1;
+
+   ListaPalabra palabra2 = new nodoPalabra;
+   palabra2->palabra = "Perro";
+   palabra2->sigPalabra = NULL;
+
+   palabra1->sigPalabra = palabra2;
+
+
 }
